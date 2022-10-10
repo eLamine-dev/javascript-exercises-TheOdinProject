f72babb173dfe90b51abe77eae4c4d37053e1910
e-lamine <lamine@e-lamine.dev>
2022-10-10 11:13:56 +0100

    all tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const ftoc = function() {
+const ftoc = function(x) {
+
+let raw = (x-32) * (5/9);
+let result = parseFloat(raw.toFixed(1));
+return result;
 
 };
 
-const ctof = function() {
+const ctof = function(x) {
+
+  let raw = x *(9/5)+32;
+  let result = parseFloat(raw.toFixed(1));
+  return result;
 
 };
 
