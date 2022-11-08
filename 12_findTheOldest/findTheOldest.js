ab72722ac938e3cb72527bc221f48b43eb4c8b5d
e-lamine <lamine@e-lamine.dev>
2022-11-08 16:30:45 +0100

    solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let sorted = people.sort((a, b)=>{
+        let currentYear = new Date().getFullYear();
+        let aLived = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+        let bLived = (b.yearOfDeath || currentYear) - b.yearOfBirth;
+        if (aLived > bLived) return -1;
+        if (aLived < bLived) return 1;
+    })
 
+    return sorted[0];
 };
 
 // Do not edit below this line
