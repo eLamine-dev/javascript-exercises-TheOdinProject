a12c78a51c5e9387a9abc8070fbcc0d02e3bcb83
e-lamine <lamine@e-lamine.dev>
2022-11-08 10:41:07 +0100

    my solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return 'OOPS'
+    let fibonacciArray = [1];
+    for (let i = 1; i < n; i++) {
+    fibonacciArray.push(fibonacciArray[i-1] + (fibonacciArray[i-2] || 0));
+   }
+   return fibonacciArray[n-1]
 };
 
 // Do not edit below this line
