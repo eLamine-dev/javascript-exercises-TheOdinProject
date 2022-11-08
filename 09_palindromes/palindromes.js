288e58f0abc8a40a5adf3ae1e1a6203e47fa68bc
e-lamine <lamine@e-lamine.dev>
2022-11-08 08:41:26 +0100

    my solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLocaleLowerCase();
+    let strForward = '';
+    let strBackward = '';
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i).match(/[a-z]/g)){
+            strForward = strForward + str.charAt(i);
+        }
+    }
+    for (let i = str.length; i >= 0; i--) {
+        if (str.charAt(i).match(/[a-z]/g)){
+            strBackward = strBackward + str.charAt(i);
+        }
+    }
+    
+    return strForward === strBackward;
 };
 
 // Do not edit below this line
